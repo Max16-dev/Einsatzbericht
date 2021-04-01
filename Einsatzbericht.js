846d88560af2eacc2996947df381a884fd989f1c
Max16-dev <75421642+Max16-dev@users.noreply.github.com>
2021-04-01 20:39:47 +0200

    Add files via upload

--- Einsatzbericht.js
+++ Einsatzbericht.js
@@ -2,4 +2,37 @@ window.onload = function() {
     
 }
 
-
+function changeEinsatzTyp(){
+    var selectElement = document.getElementById("einsatzArt").value;
+    var anzeigeTypBrand = document.getElementById("anzeigeTypBrand");
+    var anzeigeTypBrandmeldeanlage = document.getElementById("anzeigeTypBrandmeldeanlage");
+    var anzeigeTypTechnischeHilfeleistung = document.getElementById("anzeigeTypTechnischeHilfeleistung");
+    
+    switch(selectElement){
+        case "Brand":
+            anzeigeTypBrand.removeAttribute("hidden");
+            anzeigeTypTechnischeHilfeleistung.setAttribute("hidden", true);
+            anzeigeTypBrandmeldeanlage.setAttribute("hidden", true);
+            break;
+        case "Brandmeldeanlage":
+            anzeigeTypBrandmeldeanlage.removeAttribute("hidden");
+            anzeigeTypTechnischeHilfeleistung.setAttribute("hidden", true);
+            anzeigeTypBrand.setAttribute("hidden", true);
+            break;
+        case "Technische Hilfeleistung":
+            anzeigeTypTechnischeHilfeleistung.removeAttribute("hidden");
+            anzeigeTypBrand.setAttribute("hidden", true);
+            anzeigeTypBrandmeldeanlage.setAttribute("hidden", true);
+            break;
+        case "Alles anzeigen":
+            anzeigeTypBrand.removeAttribute("hidden");
+            anzeigeTypBrandmeldeanlage.removeAttribute("hidden", true);
+            anzeigeTypTechnischeHilfeleistung.removeAttribute("hidden");
+            break;
+        case "Auswählen":
+            anzeigeTypBrand.setAttribute("hidden", true);
+            anzeigeTypTechnischeHilfeleistung.setAttribute("hidden", true);
+            anzeigeTypBrand.setAttribute("hidden", true);
+            anzeigeTypBrandmeldeanlage.setAttribute("hidden", true);
+    }
+}
