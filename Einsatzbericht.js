fd49b3f63695f36cbef3e5177b434565d41db60b
Max16-dev <75421642+Max16-dev@users.noreply.github.com>
2021-04-02 11:47:26 +0200

    Add files via upload

--- Einsatzbericht.js
+++ Einsatzbericht.js
@@ -7,32 +7,44 @@ function changeEinsatzTyp(){
     var anzeigeTypBrand = document.getElementById("anzeigeTypBrand");
     var anzeigeTypBrandmeldeanlage = document.getElementById("anzeigeTypBrandmeldeanlage");
     var anzeigeTypTechnischeHilfeleistung = document.getElementById("anzeigeTypTechnischeHilfeleistung");
-    
+    var anzeigeTypSonstiges = document.getElementById("anzeigeTypSonstiges");
+
     switch(selectElement){
         case "Brand":
             anzeigeTypBrand.removeAttribute("hidden");
             anzeigeTypTechnischeHilfeleistung.setAttribute("hidden", true);
             anzeigeTypBrandmeldeanlage.setAttribute("hidden", true);
+            anzeigeTypSonstiges.setAttribute("hidden", true);
             break;
         case "Brandmeldeanlage":
             anzeigeTypBrandmeldeanlage.removeAttribute("hidden");
             anzeigeTypTechnischeHilfeleistung.setAttribute("hidden", true);
             anzeigeTypBrand.setAttribute("hidden", true);
+            anzeigeTypSonstiges.setAttribute("hidden", true);
             break;
         case "Technische Hilfeleistung":
             anzeigeTypTechnischeHilfeleistung.removeAttribute("hidden");
             anzeigeTypBrand.setAttribute("hidden", true);
             anzeigeTypBrandmeldeanlage.setAttribute("hidden", true);
+            anzeigeTypSonstiges.setAttribute("hidden", true);
             break;
         case "Alles anzeigen":
             anzeigeTypBrand.removeAttribute("hidden");
             anzeigeTypBrandmeldeanlage.removeAttribute("hidden", true);
             anzeigeTypTechnischeHilfeleistung.removeAttribute("hidden");
+            anzeigeTypSonstiges.removeAttribute("hidden", true);
+            break;
+        case "Sonstige Einsätze":
+            anzeigeTypTechnischeHilfeleistung.setAttribute("hidden", true);
+            anzeigeTypBrand.setAttribute("hidden", true);
+            anzeigeTypBrandmeldeanlage.setAttribute("hidden", true);
+            anzeigeTypSonstiges.removeAttribute("hidden");
             break;
         case "Auswählen":
             anzeigeTypBrand.setAttribute("hidden", true);
             anzeigeTypTechnischeHilfeleistung.setAttribute("hidden", true);
             anzeigeTypBrand.setAttribute("hidden", true);
             anzeigeTypBrandmeldeanlage.setAttribute("hidden", true);
+            anzeigeTypSonstiges.setAttribute("hidden", true);
     }
 }
