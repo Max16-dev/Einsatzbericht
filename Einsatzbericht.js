210fbf38289465e0916889c483885456fc882e78
Max16-dev <75421642+Max16-dev@users.noreply.github.com>
2021-04-03 20:26:02 +0200

    Add files via upload

--- Einsatzbericht.js
+++ Einsatzbericht.js
@@ -1,5 +1,5 @@
 window.onload = function() {
-    
+    document.getElementById("TaetigkeitDerFeuerwehrMitBehandlungVerunglueckter").value = "\n" + "Einsatzende";
 }
 
 function changeEinsatzTyp(){
@@ -47,4 +47,48 @@ function changeEinsatzTyp(){
             anzeigeTypBrandmeldeanlage.setAttribute("hidden", true);
             anzeigeTypSonstiges.setAttribute("hidden", true);
     }
+}
+
+function TextBetreffenenObjekt(){
+    var text = document.getElementById("WasHatGebranntMitObjektUndLage");
+    var addTextBMAMelder = document.getElementById("addTextBMAMelder").checked;
+    if(addTextBMAMelder == true && (!text.value.includes("Ausgelöst"))){
+        text.value += "Ausgelöst hat die Meldergruppe x Melder x im x Geschoss / Gebäude x ";
+    }
+}
+
+function TaetigkeitDerFeuerwehrMitBehandlungVerunglueckter(){
+    var text = document.getElementById("TaetigkeitDerFeuerwehrMitBehandlungVerunglueckter");
+    var addTextBMAErkundung = document.getElementById("addTextBMAErkundung").checked;
+    var addTextBMAAusloesungBauarbeiten = document.getElementById("addTextBMAAusloesungBauarbeiten").checked;
+    var addTextBMAAusloesungNichtErsichtlich = document.getElementById("addTextBMAAusloesungNichtErsichtlich").checked;
+    var addTextBMABetreiberAusserBetrieb = document.getElementById("addTextBMABetreiberAusserBetrieb").checked;
+    var addTextBMAFeuerwehrZurueckgestellt = document.getElementById("addTextBMAFeuerwehrZurueckgestellt").checked;
+    var einsatzEnde = "\n" + "Einsatzende";
+
+    if(addTextBMAErkundung == true && (!text.value.includes("Erkundung"))){
+        text.value = text.value.replace(einsatzEnde, "");
+        text.value += "Nach der Erkundung wurde festgestellt, dass es sich um ein Fehlalarm handelte. " + "\n" + "Einsatzende";
+    }
+    if(addTextBMAAusloesungBauarbeiten == true && (!text.value.includes("Bauarbeiten"))){
+        text.value = text.value.replace(einsatzEnde, "");
+        text.value += "Der Auslösegrund waren Bauarbeiten. " + "\n" + "Einsatzende";
+    }
+    if(addTextBMAAusloesungNichtErsichtlich == true && (!text.value.includes("nicht ersichtlich"))){
+        text.value = text.value.replace(einsatzEnde, "");
+        text.value += "Der Auslösegrund war nicht ersichtlich. " + "\n" + "Einsatzende";
+    }
+    if(addTextBMABetreiberAusserBetrieb == true && (!text.value.includes("Betreiber"))){
+        text.value = text.value.replace(einsatzEnde, "");
+        text.value += "Der defekte Melder wurde vom Betreiber außer Betrieb genommen. " + "\n" + "Einsatzende";
+    }
+    if(addTextBMAFeuerwehrZurueckgestellt == true && (!text.value.includes("zurückgestellt"))){
+        text.value = text.value.replace(einsatzEnde, "");
+        text.value += "Die BMA wurde von der Feuerwehr mehr stummgestellt. " + "\n" + "Einsatzende";
+    }
+}
+
+function TestPDF(){
+    const text = document.getElementById("GeretteteMenschen");
+    html2pdf().from(text).save();
 }
